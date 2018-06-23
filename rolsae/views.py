8524166a66f27711aa64f87af5b51fbd833ea987
rolsae <rolsaee@gmail.com>
2018-06-23 20:24:41 +0300

    Added views to create/edit blog post inside the site.

--- rolsae/views.py
+++ rolsae/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Question, Choice
+from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
@@ -23,3 +24,29 @@ def vote(request, question_id):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'rolsae/post-detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'rolsae/post_edit.html', {'form': form})
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'rolsae/post_edit.html', {'form': form})
